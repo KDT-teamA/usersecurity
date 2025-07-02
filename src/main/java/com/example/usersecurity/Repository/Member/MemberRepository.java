94726b185b91c741238088a65f7ca1e32504f898
tatoon0 <qudwjd4296@naver.com>
2025-07-02 12:52:39 +0900

    commit

--- src/main/java/com/example/usersecurity/Repository/Member/MemberRepository.java
+++ src/main/java/com/example/usersecurity/Repository/Member/MemberRepository.java
@@ -1,4 +1,12 @@
 package com.example.usersecurity.Repository.Member;
 
-public interface MemberRepository {
+import com.example.usersecurity.Entity.Member.MemberEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<MemberEntity, Integer> {
+    //사용자 id로 조회
+    Optional<MemberEntity> findByUserid(String userid);
 }
