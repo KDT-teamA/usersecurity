0e37489802408624cecd98acf7a634c40eefb0e1
tatoon0 <qudwjd4296@naver.com>
2025-07-02 12:20:49 +0900

    commit

--- src/main/java/com/example/usersecurity/Constant/Level.java
+++ src/main/java/com/example/usersecurity/Constant/Level.java
@@ -1,4 +1,12 @@
 package com.example.usersecurity.Constant;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Level {
+    ADMIN("관리자"), OPERATOR("운영자"), USER("사용자");
+
+    private final String description;
 }
